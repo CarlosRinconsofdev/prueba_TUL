e889672970d64330557578e265b455edc8715170
carlosra888 <3901@holbertonschool.com>
2023-09-06 12:59:01 -0500

    fix code

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 100000
 
-char string [MAX] = {0}; //string a manipular
+char string [MAX]; //string a manipular
 __uint32_t n = 0; // Longitud del string
 __uint32_t k = 0; // Intentos
-//__uint16_t pm = 0; // Parejas a modificar
+struct parejasM p;
+char s[MAX] = {'8'};
 
 struct parejasM {
     __uint16_t PM;
-    char posicionesM [50000];
+    __uint16_t posicionesM [50000];
 }; 
 
-struct parejasM p;
-char str[MAX];
+
 
 struct parejasM parejas_modificar(char str[]){
     struct parejasM p;
+    p.PM = 0;
     __uint32_t j = n / 2;
     for(__uint32_t i = 0; i < j; i++){
         if (str[i] != str[n-i-1]){
@@ -27,41 +29,44 @@ struct parejasM parejas_modificar(char str[]){
     return p;
 }
 
-char* highestValuePalindrome (char str[], struct parejasM pm){
-    char* s;
-    if (k <= pm.PM){
-        s = "-1";
+char* highestValuePalindrome (char str[MAX], struct parejasM pm){
+
+    if (k < pm.PM){
+        strcpy(s, "-1");
     }
     else if(k == pm.PM){
         for(__uint16_t i = 0; i <= pm.PM; i++){
-            if (str[pm.posicionesM[i]] > str[pm.posicionesM[n-i-1]]){
-                str[pm.posicionesM[n-i-1]] = str[pm.posicionesM[i]];
+            if (str[pm.posicionesM[i]] > str[n - pm.posicionesM[i] - 1]){
+                str[n - pm.posicionesM[i] - 1] = str[pm.posicionesM[i]];
             }
             else {
-                str[pm.posicionesM[i]] = str[pm.posicionesM[n-i-1]];
+                str[pm.posicionesM[i]] = str[(n - pm.posicionesM[i] - 1)];
             }
             
         }
-        s = &str;
+        strcpy(s, str);
+    }
+    else {
+        // No me alcanzo el tiempo para implementar esta funcion.
     }
     return s;
 }
 
 int main() {
-    // printf() displays the string inside quotation
-    printf("Ingrese el valor de k y n separados por espacio\n");
+
+    printf("Ingrese el valor de n y k separados por espacio\n");
     scanf("%i %i", &n, &k);
     printf("Ingrese s\n");
     scanf("%s", &string);
-    //fgets(string, MAX, stdin);
     printf("Usted acaba de ingresar:\n");
     printf("n = %i k = %i\n", n, k);
     printf("s = '%s'\n", string);
-    //printf("s = '%c'\n", string[9]);
     p = parejas_modificar(string);
     printf("Parejas a modificar = %d\n", p.PM);
-    char* s = highestValuePalindrome(string, p);
-    strcpy(string, s);
-    printf("Programa finalizado con: %s\n", string);
+    highestValuePalindrome(string, p);
+    //printf("La primera pareja a cambiar esta en la posicion: %d\n", (p.posicionesM[0]));
+    printf("Programa finalizado con: %s\n", s);
+
     return 0;
+
 }
